9f588a0c72648ee7357358019787eb8df2899e44
Lissu <mary567@hanmail.net>
2022-08-03 17:29:01 +0900

    wip infinite

--- src/components/List/InfiniteScroll/InfiniteScroll.tsx
+++ src/components/List/InfiniteScroll/InfiniteScroll.tsx
@@ -1,6 +1,20 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+
 import { InfiniteScrollWrapper } from './InfiniteScrollStyle';
+import { getProductList } from '../../../api/productApi';
 
 const InfiniteScroll = () => {
+
+  useInfiniteQuery(['infinite'],
+    ({ pageParam = 0 }: QueryFunctionContext) =>
+      axios.get<PaginationResponse<User>>('/users', {
+        params: { page: pageParam, size },
+      }),
+    {
+      getNextPageParam: ({ data: { isLastPage, pageNumber } }) =>
+        isLastPage ? undefined : pageNumber + 1,
+    }
+
   return <InfiniteScrollWrapper></InfiniteScrollWrapper>;
 };
 
